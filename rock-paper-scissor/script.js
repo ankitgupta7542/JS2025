ef915f98ab108f16bb7f792fedb56b4dda542397
ankitgupta7542 <ankitgupta7542@gmail.com>
2025-03-23 00:52:03 +0530

    funtionality changed

--- rock-paper-scissor/script.js
+++ rock-paper-scissor/script.js
@@ -1,48 +1,85 @@
-function ComputerMove(){
-    let random = Math.random()*3;
-    if(random > 0 && random <= 1)
+function ComputerMove() {
+    let random = Math.random() * 3;
+    if (random > 0 && random <= 1)
         return "Rock";
-    else if(random > 1 && random <= 2)
+    else if (random > 1 && random <= 2)
         return "Paper";
     else
         return "Scissor";
 }
 
-function result(userMove , ComputerMove){
-    if(userMove === "Rock")
-    {
-        if(ComputerMove === "Rock")
+let score = {
+    win: 0,
+    loss: 0,
+    tie: 0,
+};
+
+function result(userMove, ComputerMove) {
+    if (userMove === "Rock") {
+        if (ComputerMove === "Rock") {
+            score.tie++;
             return "its a tie.";
-        else if(ComputerMove === "Paper")
+        }
+        else if (ComputerMove === "Paper") {
+            score.loss++;
             return "Computer won.";
-        else
+        }
+        else {
+            score.win++;
             return "User won.";
+        }
     }
 
-    else if(userMove === "Paper")
-    {
-        if(ComputerMove === "Paper")
+    else if (userMove === "Paper") {
+        if (ComputerMove === "Paper") {
+            score.tie++;
             return "its a tie.";
-        else if(ComputerMove === "Rock")
+        }
+        else if (ComputerMove === "Rock") {
+            score.win++;
             return "User won.";
-        else
-        return "Computer won.";
+        }
+        else {
+            score.loss++;
+            return "Computer won.";
+        }
     }
 
-    else
-    {
-        if(ComputerMove === "Scissor")
+    else {
+        if (ComputerMove === "Scissor") {
+            score.tie++;
             return "its a tie.";
-        else if(ComputerMove === "Paper")
+        }
+        else if (ComputerMove === "Paper") {
+            score.win++;
             return "User won.";
-        else
+        }
+        else {
+            score.loss++;
             return "Computer won.";
+        }
     }
 }
 
+console.log(score);
 
-function showResult(userMove , ComputerMove , result){
-    return `You have choosen ${userMove} , Computer has choosen ${ComputerMove} and The result is ${result}.`;
+// function showResult(userMove , ComputerMove , result){
+//     console.log(score);
+//     // alert(`score.win`);
+
+//     // return alert(`You have choosen ${userMove} , Computer has choosen ${ComputerMove} and The result is ${result}.`);
+// //    return document.querySelector('.user-move').innerText = `You have choosen ${userMove}`;
+// }
+
+function displayScore(userMove, ComputerMove, result) {
+    document.querySelector('.user-move').innerText = `User has choosen: ${userMove}`;
+    document.querySelector('.computer-move').innerText = `Computer has choosen: ${ComputerMove}`;
+    document.querySelector('.result').innerText = `Result : ${result}`;
+}
+
+function scoreCount(win , loss , tie)
+{
+    document.querySelector('.scoreCount').innerText = `Win : ${win}, Loss : ${loss},  Tie : ${tie}`;
 }
 
 
